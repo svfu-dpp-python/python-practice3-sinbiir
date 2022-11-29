252699faa95b71df9329278b27cf975429701d2f
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-11-29 21:37:48 +0900

    Fix

--- tests/test_product.py
+++ tests/test_product.py
@@ -4,18 +4,30 @@ from product import Product
 
 
 class TestProduct(unittest.TestCase):
+
     def test_name(self):
         name = 'Товар'
         p = Product(name, 1.0)
         self.assertEqual(p.name, name)
+
+    def test_price(self):
+        price = 1.0
+        p = Product('Товар', price)
+        self.assertEqual(p.get_price(), price)
 
     def test_positive_price(self):
         new_price = 5.0
         p = Product('Товар', 1.0)
         p.set_price(new_price)
         self.assertEqual(p.get_price(), new_price)
+
+    def test_price_not_readable_with_class(self):
         with self.assertRaises(TypeError):
             Product.get_price()
+
+    def test_price_not_writable_with_class(self):
+        with self.assertRaises(TypeError):
+            Product.set_price(1.0)
 
     def test_zero_price(self):
         old_price = 1.0
@@ -49,7 +61,12 @@ class TestProduct(unittest.TestCase):
 
     def test_negative_discount(self):
         old_discount = 0.0
-        p = Product('Товар', old_discount)
+        p = Product('Товар', 1.0)
         p.set_discount(-5.0)
         self.assertEqual(p.get_discount(), old_discount)
         self.assertEqual(Product.get_discount(), old_discount)
+
+    def test_property(self):
+        p = Product('Товар', 100.0)
+        p.set_discount(10.0)
+        self.assertAlmostEqual(p.current_price, 90.0)
